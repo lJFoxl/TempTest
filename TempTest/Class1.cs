261f704f7436dc1d79d1418ab6c393e535a43771
ljfoxl <inbokov@gmail.com>
2023-10-30 13:38:20 +0300

    add class SingleProjectTest

--- TempTest/Class1.cs
+++ TempTest/Class1.cs
@@ -11,7 +11,7 @@ namespace TempTest;
 [AllureNUnit]
 [TestFixture]
 [Parallelizable(ParallelScope.All)]
-public class Class1:Test
+public class Class1: SingleProjectTest
 {
     [OneTimeSetUp]
     public void OneTimeSetup()
@@ -34,5 +34,19 @@ public class Class1:Test
     public void Test()
     {
         Assert.True(true);
+    }
+}
+[SetUpFixture]
+public class SingleProjectTest
+{
+    [OneTimeSetUp]
+    public void OneTimeSetup()
+    {
+        var path = Path.Combine(AppContext.BaseDirectory, @"TestPath");
+        var fileConfig = new DirectoryInfo(path).GetFiles().FirstOrDefault();
+        Assert.IsNotNull(fileConfig);
+        Assert.AreEqual(fileConfig.Name, AllureConstants.CONFIG_FILENAME);
+        path = Path.Combine(path, AllureConstants.CONFIG_FILENAME);
+        Environment.SetEnvironmentVariable(AllureConstants.ALLURE_CONFIG_ENV_VARIABLE, path, EnvironmentVariableTarget.Process);
     }
 }
